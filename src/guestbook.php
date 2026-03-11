c61228bfa04dee6486f174e04dbb3955939c3f4c
AL-student-11 <255115666+AL-student-11@users.noreply.github.com>
2026-03-11 14:55:04 +0200

    Change language of content in guestbook to English

--- src/guestbook.php
+++ src/guestbook.php
@@ -1,32 +1,32 @@
 <!DOCTYPE html>
-<html lang="fi">
+<html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="Vieraskirja">
-	<title>Vieraskirja</title>
+	<meta name="description" content="guestbook">
+	<title>Health Fitness Plus's Guestbook</title>
 </head>
 <body>
 	<div class="entry">
 		<form action="save_entry.php" method="post">
 			<input type=text name="datetime" hidden>
-			<label for="name">Nimesi: </label>
+			<label for="name">Name: </label>
 			<input id="name" name="name" type=text required>
-			<label for="email">Sähköposti: </label>
+			<label for="email">Email: </label>
 			<input id="email" name="email" type=text>
-			<label for="message"></label>
+			<label for="message">Message</label>
 			<textarea id="message" name="message" rows=5></textarea>
-			<button type="submit">Lähetä</button>
+			<button type="submit">Submit</button>
 		</form>
 	</div>
 	<div class="entries">
 		<table>
-			<caption>Sivulla vierailleet</caption>
+			<caption>Guestbook entries</caption>
 			<tr>
-				<th>Aika</th>
-				<th>Nimi</th>
+				<th>Time</th>
+				<th>Name</th>
 				<th>Email</th>
-				<th>Viesti</th>
+				<th>Message</th>
 			</tr>
 		</table>
 	</div>
