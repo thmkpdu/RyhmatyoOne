136b2610861dac2d5016c965aff6c9da4e05bf75
AL-student-11 <255115666+AL-student-11@users.noreply.github.com>
2026-04-30 17:52:28 +0300

    Remove variables

--- src/session.php
+++ src/session.php
@@ -1,8 +1,6 @@
 <?php
 
 $timeout = 60;
-$user_hash = '$2y$15$0n82r6J.ZfOwcnZIK/dvbO9JsMlBkR.rG0bmPcgbGd4vNnkeY6v8W';
-$pass_hash = '$2y$15$1FSwxnAeM83EsYOQhFvWD.o32wnnC3nUhiiwAu66CpoUG38r0inCW';
 
 function session_is_valid() {
 	global $timeout;
