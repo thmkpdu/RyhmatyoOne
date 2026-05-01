00c81624c798d22e3f81d383fa7f479e7b04c2c9
AL-student-11 <255115666+AL-student-11@users.noreply.github.com>
2026-05-01 23:55:24 +0300

    Change php output to htmlspecialchars()

--- src/settimes.php
+++ src/settimes.php
@@ -12,6 +12,7 @@
 	$ERROR_HTML .= '</html>';
 
 	if(!session_is_valid()) {
+		error_log($_SERVER['PHP_SELF'] . ": Session not valid", 0);
 		$_SESSION = [];
 		session_destroy();
 		echo $ERROR_HTML;
@@ -32,9 +33,9 @@
 			$data[] = [$row["open"], $row["opentime"], $row["closetime"]];
 		}
 	} catch(mysqli_sql_exception $e) {
-		error_log($e->getMessage(), 0);
+		error_log($_SERVER['PHP_SELF'] . ": " . $e->getMessage(), 0);
 	} catch(Throwable $t) {
-		error_log($t->getMessage(), 0);
+		error_log($_SERVER['PHP_SELF'] . ": " . $t->getMessage(), 0);
 		$dataset = false; // Make sure variable exist
 	}
 ?>
@@ -51,7 +52,7 @@
 	<?php echo "<p>Hello " . $_SESSION["admin"] . ":" . $_SESSION["stamp"] . ":" . session_id() . "</p>";?>
     <div id="header">
         <h1>Set opening and closing times</h1>
-        <button id="btnLogout">Log out</button>
+        <button id="btnLogout" onclick="window.location.href='logout.php'">Log out</button>
     </div>
     <div id="descriptions">
         <p>Is open</p>
@@ -61,50 +62,50 @@
     </div>
     <hr width="100%" size="2" color="black">
     <div id="weekdayInputs">
-        <form method="post" id="weekdayForm">
+        <form action="savetimes.php" method="post" id="weekdayForm">
             <div id="divMon" class="dayDiv">
-                <input type="checkbox" id="chbMon" name="Mon" <?php if($dataset) echo $data[0][0] ? "checked":""; ?>>
+                <input type="checkbox" id="chbMon" name="Mon" <?php if($dataset) echo $data[0][0] ? "checked":"";?>>
                 <p>Monday</p>
-                <input name="MonOpen" type=time <?php if($dataset) echo '"' . $data[0][1] . '"';?>>
-                <input name="MonClose" type=time <?php if($dataset) echo '"' . $data[0][2] . '"';?>>
+                <input name="MonOpen" type=time value=<?php if($dataset) echo '"' . htmlspecialchars($data[0][1]) . '"';?>>
+                <input name="MonClose" type=time value=<?php if($dataset) echo '"' . htmlspecialchars($data[0][2]) . '"';?>>
             </div>
             <div id="divTue" class="dayDiv">
-                <input type="checkbox" id="chbTue" name="Tue" <?php if($dataset) echo $data[1][0] ? "checked":""; ?>>
+                <input type="checkbox" id="chbTue" name="Tue" <?php if($dataset) echo $data[1][0] ? "checked":"";?>>
                 <p>Tuesday</p>
-                <input name="TueOpen" type=time <?php if($dataset) echo '"' . $data[1][1] . '"';?>>
-                <input name="TueClose" type=time <?php if($dataset) echo '"' . $data[1][2] . '"';?>>
+                <input name="TueOpen" type=time value=<?php if($dataset) echo '"' . htmlspecialchars($data[1][1]) . '"';?>>
+                <input name="TueClose" type=time value=<?php if($dataset) echo '"' . htmlspecialchars($data[1][2]) . '"';?>>
             </div>
             <div id="divWed" class="dayDiv">
-                <input type="checkbox" id="chbWed" name="Wed" <?php if($dataset) echo $data[2][0] ? "checked":""; ?>>
+                <input type="checkbox" id="chbWed" name="Wed" <?php if($dataset) echo $data[2][0] ? "checked":"";?>>
                 <p>Wednesday</p>
-                <input name="WedOpen" type=time <?php if($dataset) echo '"' . $data[2][1] . '"';?>>
-                <input name="WedClose" type=time <?php if($dataset) echo '"' . $data[2][2] . '"';?>>
+                <input name="WedOpen" type=time value=<?php if($dataset) echo '"' . htmlspecialchars($data[2][1]) . '"';?>>
+                <input name="WedClose" type=time value=<?php if($dataset) echo '"' . htmlspecialchars($data[2][2]) . '"';?>>
             </div>
             <div id="divThu" class="dayDiv">
-                <input type="checkbox" id="chbThu" name="Thu" <?php if($dataset) echo $data[3][0] ? "checked":""; ?>>
+                <input type="checkbox" id="chbThu" name="Thu" <?php if($dataset) echo $data[3][0] ? "checked":"";?>>
                 <p>Thursday</p>
-                <input name="ThuOpen" type=time <?php if($dataset) echo '"' . $data[3][1] . '"';?>>
-                <input name="ThuClose" type=time <?php if($dataset) echo '"' . $data[3][2] . '"';?>>
+                <input name="ThuOpen" type=time value=<?php if($dataset) echo '"' . htmlspecialchars($data[3][1]) . '"';?>>
+                <input name="ThuClose" type=time value=<?php if($dataset) echo '"' . htmlspecialchars($data[3][2]) . '"';?>>
             </div>
             <div id="divFri" class="dayDiv">
-                <input type="checkbox" id="chbFri" name="Fri" <?php if($dataset) echo $data[4][0] ? "checked":""; ?>>
+                <input type="checkbox" id="chbFri" name="Fri" <?php if($dataset) echo $data[4][0] ? "checked":"";?>>
                 <p>Friday</p>
-                <input name="FriOpen" type=time <?php if($dataset) echo '"' . $data[4][1] . '"';?>>
-                <input name="FriClose" type=time <?php if($dataset) echo '"' . $data[4][2] . '"';?>>>
+                <input name="FriOpen" type=time value=<?php if($dataset) echo '"' . htmlspecialchars($data[4][1]) . '"';?>>
+                <input name="FriClose" type=time value=<?php if($dataset) echo '"' . htmlspecialchars($data[4][2]) . '"';?>>
             </div>
             <div id="divSat" class="dayDiv">
-                <input type="checkbox" id="chbSat" name="Sat" <?php if($dataset) echo $data[5][0] ? "checked":""; ?>>
+                <input type="checkbox" id="chbSat" name="Sat" <?php if($dataset) echo $data[5][0] ? "checked":"";?>>
                 <p>Saturday</p>
-                <input name="SatOpen" type=time <?php if($dataset) echo '"' . $data[5][1] . '"';?>>
-                <input name="SatClose" type=time <?php if($dataset) echo '"' . $data[5][2] . '"';?>>
+                <input name="SatOpen" type=time value=<?php if($dataset) echo '"' . htmlspecialchars($data[5][1]) . '"';?>>
+                <input name="SatClose" type=time <?php if($dataset) echo '"' . htmlspecialchars($data[5][2]) . '"';?>>
             </div>
             <div id="divSun" class="dayDiv">
-                <input type="checkbox" id="chbSun" name="Sun" <?php if($dataset) echo $data[6][0] ? "checked":""; ?>>
+                <input type="checkbox" id="chbSun" name="Sun" <?php if($dataset) echo $data[6][0] ? "checked":"";?>>
                 <p>Sunday</p>
-                <input name="SunOpen" type=time <?php if($dataset) echo '"' . $data[6][1] . '"';?>>
-                <input name="SunClose" type=time <?php if($dataset) echo '"' . $data[6][2] . '"';?>>
+                <input name="SunOpen" type=time value=<?php if($dataset) echo '"' . htmlspecialchars($data[6][1]) . '"';?>>
+                <input name="SunClose" type=time value=<?php if($dataset) echo '"' . htmlspecialchars($data[6][2]) . '"';?>>
             </div>
-            <button id="btnOk">OK</button>
+            <input type="submit" id="btnOk" value="Ok">
             <button id="btnCancel">Cancel</button>
         </form>
     </div>
