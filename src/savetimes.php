1e50b0e88af74e86208b05476bf11e82f7f86068
AL-student-11 <255115666+AL-student-11@users.noreply.github.com>
2026-05-06 22:33:25 +0300

    More inconsistent code

--- src/savetimes.php
+++ src/savetimes.php
@@ -35,106 +35,112 @@ try {
 try {
     if($_SERVER["REQUEST_METHOD"] !== "POST") throw new Exception("Unexpected HTTP request");
 
-    $closed = [false, "00:00", "00:00"];
+    $no_data = [false, "00:00", "00:00"];
 
-	if(!empty($_POST["Mon"]) && !empty($_POST["MonOpen"]) && !empty($_POST["MonClose"])) {
-		if(is_later($_POST["MonClose"], $_POST["MonOpen"])) {
-			$data[] = [true, $_POST["MonOpen"], $_POST["MonClose"]];
-		} elseif($_POST["MonOpen"] === $_POST["MonClose"]) {
+	if(!empty($_POST["MonOpen"]) && !empty($_POST["MonClose"])) {
+		if($_POST["MonOpen"] === $_POST["MonClose"]) {
 			html_direct("Refused to save. Monday's opening time and closing time are the same", "settimes.php", 3, false);
 			exit();
-		} else {
+		} elseif(is_later($_POST["MonOpen"], $_POST["MonClose"])) {
 			html_direct("Refused to save. Monday's opening time is later than closing time", "settimes.php", 3, false);
 			exit();
+		} else {
+			$open = isset($_POST["Mon"]);
+			$data[] = [$open, $_POST["MonOpen"], $_POST["MonClose"]];
 		}
 	} else {
-		$data[] = $closed;
+		$data[] = $no_data;
 	}
 
-	if(!empty($_POST["Tue"]) && !empty($_POST["TueOpen"]) && !empty($_POST["TueClose"])) {
-		if(is_later($_POST["TueClose"], $_POST["TueOpen"])) {
-			$data[] = [true, $_POST["TueOpen"], $_POST["TueClose"]];
-		} elseif($_POST["TueOpen"] === $_POST["TueClose"]) {
+	if(!empty($_POST["TueOpen"]) && !empty($_POST["TueClose"])) {
+		if($_POST["TueOpen"] === $_POST["TueClose"]) {
 			html_direct("Refused to save. Tuesday's opening time and closing time are the same", "settimes.php", 3, false);
 			exit();
-		} else {
-			html_direct("Refused to save. Tueday's opening time is later than closing time", "settimes.php", 3, false);
+		} elseif(is_later($_POST["TueOpen"], $_POST["TueClose"])) {
+			html_direct("Refused to save. Tuesday's opening time is later than closing time", "settimes.php", 3, false);
 			exit();
+		} else {
+			$open = isset($_POST["Tue"]);
+			$data[] = [$open, $_POST["TueOpen"], $_POST["TueClose"]];
 		}
 	} else {
-		$data[] = $closed;
+		$data[] = $no_data;
 	}
 
-	if(!empty($_POST["Wed"]) && !empty($_POST["WedOpen"]) && !empty($_POST["WedClose"])) {
-		if(is_later($_POST["WedClose"], $_POST["WedOpen"])) {
-			$data[] = [true, $_POST["WedOpen"], $_POST["WedClose"]];
-		} elseif($_POST["WedOpen"] === $_POST["WedClose"]) {
+	if(!empty($_POST["WedOpen"]) && !empty($_POST["WedClose"])) {
+		if($_POST["WedOpen"] === $_POST["WedClose"]) {
 			html_direct("Refused to save. Wednesday's opening time and closing time are the same", "settimes.php", 3, false);
 			exit();
-		} else {
+		} elseif(is_later($_POST["WedOpen"], $_POST["WedClose"])) {
 			html_direct("Refused to save. Wednesday's opening time is later than closing time", "settimes.php", 3, false);
 			exit();
+		} else {
+			$open = isset($_POST["Wed"]);
+			$data[] = [$open, $_POST["WedOpen"], $_POST["WedClose"]];
 		}
 	} else {
-		$data[] = $closed;
+		$data[] = $no_data;
 	}
 
-	if(!empty($_POST["Thu"]) && !empty($_POST["ThuOpen"]) && !empty($_POST["ThuClose"])) {
-		if(is_later($_POST["ThuClose"], $_POST["ThuOpen"])) {
-			$data[] = [true, $_POST["ThuOpen"], $_POST["ThuClose"]];
-		} elseif($_POST["ThuOpen"] === $_POST["ThuClose"]) {
-			html_direct("Refused to save. Thurday's opening time and closing time are the same", "settimes.php", 3, false);
+	if(!empty($_POST["ThuOpen"]) && !empty($_POST["ThuClose"])) {
+		if($_POST["ThuOpen"] === $_POST["ThuClose"]) {
+			html_direct("Refused to save. Thursday's opening time and closing time are the same", "settimes.php", 3, false);
 			exit();
-		} else {
+		} elseif(is_later($_POST["ThuOpen"], $_POST["ThuClose"])) {
 			html_direct("Refused to save. Thursday's opening time is later than closing time", "settimes.php", 3, false);
 			exit();
+		} else {
+			$open = isset($_POST["Thu"]);
+			$data[] = [$open, $_POST["ThuOpen"], $_POST["ThuClose"]];
 		}
 	} else {
-		$data[] = $closed;
+		$data[] = $no_data;
 	}
 
-	if(!empty($_POST["Fri"]) && !empty($_POST["FriOpen"]) && !empty($_POST["FriClose"])) {
-		if(is_later($_POST["FriClose"], $_POST["FriOpen"])) {
-			$data[] = [true, $_POST["FriOpen"], $_POST["FriClose"]];
-		} elseif($_POST["FriOpen"] === $_POST["FriClose"]) {
+	if(!empty($_POST["FriOpen"]) && !empty($_POST["FriClose"])) {
+		if($_POST["FriOpen"] === $_POST["FriClose"]) {
 			html_direct("Refused to save. Friday's opening time and closing time are the same", "settimes.php", 3, false);
 			exit();
-		} else {
+		} elseif(is_later($_POST["FriOpen"], $_POST["FriClose"])) {
 			html_direct("Refused to save. Friday's opening time is later than closing time", "settimes.php", 3, false);
 			exit();
+		} else {
+			$open = isset($_POST["Fri"]);
+			$data[] = [$open, $_POST["FriOpen"], $_POST["FriClose"]];
 		}
 	} else {
-		$data[] = $closed;
+		$data[] = $no_data;
 	}
 
-	if(!empty($_POST["Sat"]) && !empty($_POST["SatOpen"]) && !empty($_POST["SatClose"])) {
-		if(is_later($_POST["SatClose"], $_POST["SatOpen"])) {
-			$data[] = [true, $_POST["SatOpen"], $_POST["SatClose"]];
-		} elseif($_POST["SatOpen"] === $_POST["SatClose"]) {
+	if(!empty($_POST["SatOpen"]) && !empty($_POST["SatClose"])) {
+		if($_POST["SatOpen"] === $_POST["SatClose"]) {
 			html_direct("Refused to save. Saturday's opening time and closing time are the same", "settimes.php", 3, false);
 			exit();
-		} else {
+		} elseif(is_later($_POST["SatOpen"], $_POST["SatClose"])) {
 			html_direct("Refused to save. Saturday's opening time is later than closing time", "settimes.php", 3, false);
 			exit();
+		} else {
+			$open = isset($_POST["Sat"]);
+			$data[] = [$open, $_POST["SatOpen"], $_POST["SatClose"]];
 		}
 	} else {
-		$data[] = $closed;
+		$data[] = $no_data;
 	}
 
-	if(!empty($_POST["Sun"]) && !empty($_POST["SunOpen"]) && !empty($_POST["SunClose"])) {
-		if(is_later($_POST["SunClose"], $_POST["SunOpen"])) {
-			$data[] = [true, $_POST["SunOpen"], $_POST["SunClose"]];
-		} elseif($_POST["SunOpen"] === $_POST["SunClose"]) {
+	if(!empty($_POST["SunOpen"]) && !empty($_POST["SunClose"])) {
+		if($_POST["SunOpen"] === $_POST["SunClose"]) {
 			html_direct("Refused to save. Sunday's opening time and closing time are the same", "settimes.php", 3, false);
 			exit();
-		} else {
+		} elseif(is_later($_POST["SunOpen"], $_POST["SunClose"])) {
 			html_direct("Refused to save. Sunday's opening time is later than closing time", "settimes.php", 3, false);
 			exit();
+		} else {
+			$open = isset($_POST["Sun"]);
+			$data[] = [$open, $_POST["SunOpen"], $_POST["SunClose"]];
 		}
 	} else {
-		$data[] = $closed;
+		$data[] = $no_data;
 	}
-
 } catch (Exception $e) {
     error_log($e->getMessage(), 0);
     session_clean_up();
